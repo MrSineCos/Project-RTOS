67f47f925bcb9918407962428f5bed918eb51b7e
hellosine <sinecoswifi@gmail.com>
2025-10-05 23:21:18 +0700

    update led_blinky.cpp

--- src/led_blinky.cpp
+++ src/led_blinky.cpp
@@ -2,33 +2,33 @@
 
 void led_blinky(void *pvParameters)
 {
-  pinMode(LED_GPIO, OUTPUT);
-  int count = 0;
+    pinMode(LED_GPIO, OUTPUT);
+    int count = 0;
 
-  while (1)
-  {
-    if (led_D3 == false)
+    while (1)
     {
-      digitalWrite(LED_GPIO, LOW); // turn the LED OFF
+        if (led_D3 == false)
+        {
+            digitalWrite(LED_GPIO, LOW); // turn the LED OFF
+        }
+        else
+        {
+            switch (count)
+            {
+            case 2:
+                count = 0;
+            case 0:
+                digitalWrite(LED_GPIO, LOW); // turn the LED OFF
+                count++;
+                break;
+            case 1:
+                digitalWrite(LED_GPIO, HIGH); // turn the LED ON
+                count++;
+                break;
+            default:
+                break;
+            }
+        }
+        vTaskDelay(1000);
     }
-    else
-    {
-      switch (count)
-      {
-      case 2:
-        count = 0;
-      case 0:
-        digitalWrite(LED_GPIO, LOW); // turn the LED OFF
-        count++;
-        break;
-      case 1:
-        digitalWrite(LED_GPIO, HIGH); // turn the LED ON
-        count++;
-        break;
-      default:
-        break;
-      }
-    }
-    vTaskDelay(1000);
-  }
 }
